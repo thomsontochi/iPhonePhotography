680c6a1d10ddbf04c37df29279b86e0351544641
austin opia <opiaaustin@gmail.com>
2023-09-02 13:29:27 +0100

    models logic

--- app/Http/Controllers/AchievementsController.php
+++ app/Http/Controllers/AchievementsController.php
@@ -9,12 +9,34 @@ class AchievementsController extends Controller
 {
     public function index(User $user)
     {
+        // dd($user);
+        // return response()->json([
+        //     'unlocked_achievements' => [],
+        //     'next_available_achievements' => [],
+        //     'current_badge' => '',
+        //     'next_badge' => '',
+        //     'remaing_to_unlock_next_badge' => 0
+        // ]);
+        // Get the user's unlocked achievements
+        $unlockedAchievements = $user->unlocked_achievements->pluck('name')->toArray();
+
+        // Get the user's current badge
+        $currentBadge = $user->getCurrentBadge();
+
+        // Get the next badge and remaining achievements to unlock it
+        $nextBadge = $user->getNextBadge();
+        $remainingToUnlockNextBadge = $user->getRemainingToUnlockNextBadge();
+
+        // Get the next available achievements for each group
+        $nextAvailableAchievements = $user->getNextAvailableAchievements();
+
+        // Return the data as a JSON response
         return response()->json([
-            'unlocked_achievements' => [],
-            'next_available_achievements' => [],
-            'current_badge' => '',
-            'next_badge' => '',
-            'remaing_to_unlock_next_badge' => 0
+            'unlocked_achievements' => $unlockedAchievements,
+            'next_available_achievements' => $nextAvailableAchievements,
+            'current_badge' => $currentBadge,
+            'next_badge' => $nextBadge,
+            'remaining_to_unlock_next_badge' => $remainingToUnlockNextBadge,
         ]);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -67,4 +67,76 @@ class User extends Authenticatable
     {
         return $this->belongsToMany(Achievement::class);
     }
+
+    public function getCurrentBadge()
+    {
+        // Logic to determine the user's current badge based on unlocked achievements
+
+        if ($this->unlocked_achievements()->count() >= 10) {
+            return 'Master';
+        } elseif ($this->unlocked_achievements()->count() >= 8) {
+            return 'Advanced';
+        } elseif ($this->unlocked_achievements()->count() >= 4) {
+            return 'Intermediate';
+        } else {
+            return 'Beginner';
+        }
+    }
+
+    public function getNextBadge()
+    {
+        // Logic to determine the user's next badge based on unlocked achievements
+
+        if ($this->unlocked_achievements()->count() >= 10) {
+            return 'Master'; // User already has the highest badge
+        } elseif ($this->unlocked_achievements()->count() >= 8) {
+            return null; // No more badges to unlock after Advanced
+        } elseif ($this->unlocked_achievements()->count() >= 4) {
+            return 'Advanced';
+        } else {
+            return 'Intermediate';
+        }
+    }
+
+    public function getRemainingToUnlockNextBadge()
+    {
+        $nextBadge = $this->getNextBadge();
+
+        if ($nextBadge) {
+            // Logic specific to your badge unlocking criteria
+            
+            $requiredAchievements = $this->getRequiredAchievementsForBadge($nextBadge);
+
+            // Calculate the remaining achievements needed for the next badge
+            $unlockedAchievements = $this->unlocked_achievements()->count();
+            $remainingToUnlockNextBadge = max(0, $requiredAchievements - $unlockedAchievements);
+
+            return $remainingToUnlockNextBadge;
+        }
+
+        return 0; // No more badges to unlock
+    }
+
+
+
+    public function getNextAvailableAchievements()
+    {
+        // Logic to determine the next available achievements for each group
+
+        $nextAvailableLessons = Achievement::whereNotIn('name', $this->unlocked_achievements->pluck('name'))
+            ->where('group', 'Lessons Watched')
+            ->pluck('name')->toArray();
+
+        $nextAvailableComments = Achievement::whereNotIn('name', $this->unlocked_achievements->pluck('name'))
+            ->where('group', 'Comments Written')
+            ->pluck('name')->toArray();
+
+
+
+        return [
+            'Lessons Watched' => $nextAvailableLessons,
+            'Comments Written' => $nextAvailableComments,
+
+        ];
+    }
 }
